4817789e27400ca56fa052af85a37c1c3f61239a
funnyling <funnyling444@gmail.com>
2016-02-21 22:31:09 +0300

    builder stub test

---
+++ builder/src/test/java/by/ntishkevich/builder/AppTest.java
@@ -0,0 +1,16 @@
+package by.ntishkevich.builder;
+
+import org.junit.Test;
+
+/**
+ * @author ntishkevich
+ * @version 21.02.2016
+ */
+public class AppTest {
+
+    @Test
+    public void appTest() {
+        String[] args = {};
+        App.main(args);
+    }
+}
